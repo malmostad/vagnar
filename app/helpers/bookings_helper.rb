ba0efabfb63c64bd27e92a040dfdccd3f528addd
martent <marten@thavenius.se>
2017-10-26 12:14:16 +0200

    Date and time_slot formatting

--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -1,5 +1,9 @@
 module BookingsHelper
-  def date_and_time_slot(booking)
-    "#{hash.from}–#{hash.to}"
+  def interval(time_slot)
+    "#{time_slot.from}–#{time_slot.to}"
+  end
+
+  def date_and_time(booking)
+    "#{booking.date} #{interval(booking.time_slot)}"
   end
 end
