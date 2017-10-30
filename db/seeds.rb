d4496821436328ebe5b84846fdbce6f74a3ea6b7
martent <marten@thavenius.se>
2017-10-30 13:38:08 +0100

    Minor

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,8 +32,7 @@ Place.create(name: 'Smoothiehörnan', address: 'Storgatan 1')
   { from: '06.00', to: '10.00' },
   { from: '10.00', to: '15.00' },
   { from: '15.00', to: '20.00' },
-  { from: '20.00', to: '24.00' },
-  { from: '24.00', to: '04.00' }
+  { from: '20.00', to: '24.00' }
 ].each do |time_slot|
   TimeSlot.create(time_slot)
 end
