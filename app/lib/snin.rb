0c3daae99a18395e02ea3264dda77bb3148f6482
martent <marten@thavenius.se>
2017-11-01 10:55:46 +0100

    Added extraction of birthday and extension for snin

--- app/lib/snin.rb
+++ app/lib/snin.rb
@@ -1,123 +1,75 @@
  # Validates different aspects of a Swedish national identification number, SNIN.
 # aka "personnummer".
-# Takes 10 or 12 digits with or without dashes
-#
-# ==== Examples:
-#    snin = Snin.new('19700101-1233')
-#    snin.valid? # => true
-#    snin.valid_control_digit # => 3
-#    snin.years_old # => Some int
-#
-#    snin = Snin.new('700101-1233')
-#    snin.add_century # => "19700101-1233"
-#    snin.valid_format?(false, true) # => true
-#
 class Snin
   # Params:
-  # +snin+:: +String+ 10 or 12 digit SNIN with or without a dashes
-  def initialize(snin)
-    @raw = snin.to_s
-    @plain = plain
+  # +raw+:: +String+ 10 or 12 digit SNIN with or without dashes
+  def initialize(raw)
+    @raw_as_digits = raw_as_digits(raw)
   end
 
   # Validates the control digit, not the specific format
   def valid?
-    to_short.size == 10 && (checksum(to_short) % 10).zero?
-  end
-
-  # Validates the format
-  #   param with_century Boolean  True if it must be 12 digit, false if 10
-  #   param with_dash Boolean  True if it must contain a dash before the last 4 digits
-  def valid_format?(with_century = false, with_dash = true)
-    date = with_century ? '\d{8}' : '\d{6}'
-    separator = with_dash ? '-' : ''
-    @raw.match(/\A#{date}#{separator}\d{4}\z/).present?
+    return 'false' if @raw_as_digits.size > 12
+    (checksum(short) % 10).zero?
   end
 
   # Returns the correct control digit
   def valid_control_digit
-    10 - checksum(to_short[0..8]) % 10
+    10 - checksum(short[0..8]) % 10
   end
 
-  def plain
-    add_century
+  def long
+    add_century_to_birthdate + extension
   end
 
-  # Returns the DateTime object for the given SNIN or false if not a valid date format
-  def to_date
-    begin
-      @plain.to_date
-    rescue
-      false
-    end
+  def short
+    long[2..-1]
   end
 
-  # Returns the age of the person as an integer
-  # Assume that the person is < 100 if SNIN is in short form
-  def years_old
-    now = Date.today
-    birthday = to_date
-    now.year - birthday.year - ((now.month > birthday.month || (now.month == birthday.month && now.day >= birthday.day)) ? 0 : 1)
+  def birthday
+    add_century_to_birthdate
   end
 
-  # Is the SNIN in long (12 digits) or short (10 digits) form?
-  def short_or_long
-    remove_dash(@raw).size == 12 ? 'long' : 'short'
-  end
-
-  # Is the SNIN on long (12 digits) form?
-  def long?
-    short_or_long == 'long'
-  end
-
-  # Is the SNIN on short (10 digits) form?
-  def short?
-    short_or_long == 'short'
-  end
-
-  def to_short
-    remove_century(remove_dash(@raw))
-  end
-
-  # Adds the century digits if missing,
-  # Assumes the person is less than 100 years old.
-  def add_century
-    raw = @raw.to_s.gsub(/\D/, '')
-    birth_date = raw[0..8].to_s
-
-    return birth_date if raw.size == 12
-
-    short_given_year = birth_date[0..1].to_i
-    today            = Date.today.to_s
-    short_year_today = today[2..3].to_i
-    century          = today[0..1].to_i
-
-    century -= 1 if short_given_year > short_year_today
-
-    raw.insert 0, century.to_s
-  end
-
-  # Removed the century digits if given in long form
-  def remove_century(val)
-    val.length == 12 ? val[2..-1] : val
-  end
-
-  # Add dash before the last 4 digits if missing
-  def add_dash(val)
-    remove_dash(val).insert -5, "-"
-  end
-
-  # Rempoves any non digit in the string
-  def remove_dash(val)
-    val.to_s.gsub(/\D/, '')
+  def extension
+    @raw_as_digits[-4..-1]
   end
 
   private
 
+  def raw_as_digits(raw)
+    raw.gsub(/\D/, '')
+  end
+
+  def remove_extension
+    @raw_as_digits[0..-5]
+  end
+
+  # Adds the century digits if missing,
+  # Assumes the person is less than 100 years old.
+  def add_century_to_birthdate
+    given_birthdate = remove_extension
+    return given_birthdate if given_birthdate.size == 8
+
+    given_year       = given_birthdate[0..1].to_i
+    short_year_today = today[2..3].to_i
+    century          = today[0..1].to_i
+
+    century -= 1 if given_year > short_year_today
+    given_birthdate.insert(0, century.to_s)
+  end
+
+  def today
+    @_today = Date.today.to_s.gsub(/\D/, '')
+  end
+
   # Calculate the checksum using the Luhn algoritm
   def checksum(val)
-    val.split(//).enum_for(:each_with_index).map do |c, i|
+    val = val.split('').each_with_index.map do |c, i|
       ((i % 2).even? ? 2 : 1) * c.to_i
-    end.to_s.split(//).inject(0) { |sum, c| sum += c.to_i }
+    end
+
+    val.to_s.split('').inject(0) do |sum, c|
+      sum + c.to_i
+    end
   end
 end
