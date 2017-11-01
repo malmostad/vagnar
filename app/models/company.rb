0146dbf43102fe5b3969d7883059938b3d757a4a
martent <marten@thavenius.se>
2017-11-01 15:10:09 +0100

    active_permit

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,10 @@
 class Company < ApplicationRecord
   has_many :sellers, dependent: :destroy
   has_many :bookings, dependent: :destroy
+
+  validates_presence_of :name, :org_number
+
+  def active_permit?
+    permit_starts_at <= Date.today && permit_ends_at >= Date.today
+  end
 end
