e0cc4d34b95f042c2cb710b0fe9a199146004fbb
martent <marten@thavenius.se>
2017-11-06 14:05:22 +0100

    Seller bookings fixes

--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -6,4 +6,12 @@ module BookingsHelper
   def date_and_time(booking)
     "#{booking.date} #{interval(booking.time_slot)}"
   end
+
+  def serving_period_interval(booking_period)
+    "#{booking_period.starts_at}–#{booking_period.ends_at}"
+  end
+
+  def booking_period_interval(booking_period)
+    "#{booking_period.booking_starts_at}–#{booking_period.booking_ends_at}"
+  end
 end
