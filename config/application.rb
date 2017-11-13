1dd71a443dcd5a6bba1e343c72b84c5f986ec9a6
martent <marten@thavenius.se>
2017-11-13 12:21:09 +0100

    Config fix

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Vagnar
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.action_view.automatically_disable_submit_tag = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
