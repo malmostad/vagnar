784d275f7d19b010ed3f8f77fca68453eb327453
martent <marten@thavenius.se>
2017-11-14 14:40:07 +0100

    Activated cookie domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,7 +2,6 @@ domain = Rails.env.production? ? 'mobilforsaljning.malmo.se' : 'local.malmo.se'
 
 Rails.application.config.session_store :cookie_store,
   key: '_vagnar_session',
-  # TODO: enable when subdomain is in place
-  # domain: domain,
+  domain: domain,
   secure: Rails.env.production? || Rails.env.test?,
   httponly: true
