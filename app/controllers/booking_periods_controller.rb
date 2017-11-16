48132123a73629eb4312b7f9a1ce7148aedc29cd
martent <marten@thavenius.se>
2017-11-16 17:25:57 +0100

    Merged controller

--- app/controllers/booking_periods_controller.rb
+++ app/controllers/booking_periods_controller.rb
@@ -39,24 +39,11 @@ class BookingPeriodsController < ApplicationController
   end
 
   private
-    def combine_datetimes
-      bpp = params[:booking_period]
-
-      params[:booking_period][:booking_starts_at] = "#{bpp[:booking_date_starts_at]} #{bpp[:booking_time_starts_at]}"
-      params[:booking_period][:booking_ends_at] = "#{bpp[:booking_date_ends_at]} #{bpp[:booking_time_ends_at]}"
-
-      %i[booking_date_starts_at booking_time_starts_at booking_date_ends_at booking_time_ends_at].each do |field|
-        params[:booking_period].delete(field)
-      end
-    end
-
     def set_booking_period
       @booking_period = BookingPeriod.find(params[:id])
     end
 
     def booking_period_params
-      combine_datetimes
-
       params.require(:booking_period).permit(
         :starts_at,
         :ends_at,
